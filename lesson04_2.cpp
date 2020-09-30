77eed7062252b8536f183d8143480c07b328f189
LeeHyeokSoo <lys8325@naver.com>
2020-10-01 02:13:45 +0900

    MaxCounters

--- lesson04_2.cpp
+++ lesson04_2.cpp
@@ -1,46 +1,43 @@
 // you can use includes, for example:
 #include <algorithm>
-#include <memory.h>
+
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
-void pr(int arr[], int l){
-    for(int i=1;i<=l;++i){
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
 vector<int> solution(int N, vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
-    vector<int> ans;
     int addList[100001] = {0,};
+    int aLen = A.size();
+    int maxCounter = N+1;
+    int idx = 0;
+    int preMax = 0;
+    int curMax = 0;
+    int target = 0;
+    vector<int> ans;
     
-     int idx=0, preMax=0, curMax=0, maxCounter=N+1;
-     int aLen = A.size();
-     int tmp;
-     for(int i=0;i<aLen;++i){
-         idx = A[i];
-         if(idx == maxCounter){
-             preMax = curMax;
-         }else{
-             tmp = addList[idx]+1;
-             if(tmp <= preMax){
-                 addList[idx] = preMax+1;
-             }else{
-                 addList[idx] = tmp;
-             }
-             curMax = max(curMax, tmp);
-         }
-     }
-     
-     for(int i=1;i<=N;++i){
-         tmp = addList[i];
-         if(tmp < preMax){
-             tmp = preMax;
-         }
-         ans.push_back(tmp);
-     }
-     
-     return ans;
+    for(int i=0;i<aLen;++i){
+        idx = A[i];
+        target = addList[idx];
+        if(idx == maxCounter){
+            preMax = curMax;
+        }else{
+            if(target < preMax){
+                addList[idx] = preMax + 1;
+            }else{
+                addList[idx] = target + 1;
+            }
+            curMax = max(addList[idx], curMax);
+        }
+    }
+    
+    for(int i=1;i<=N;++i){
+        target = addList[i];
+        if(target < preMax){
+            ans.push_back(preMax);
+        }else{
+            ans.push_back(target);
+        }
+    }
+    
+    return ans;    
 }
